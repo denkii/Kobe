a675d8668e9dd9d062f383c32790f154f478e251
denkii <bjorn@kwokinator.com>
2016-06-12 23:13:43 -0400

    Python beginning attempt

---
+++ kobe-main.py
@@ -0,0 +1,9 @@
+# Kaggle Kobe shot selection random forest classifier (Python)
+
+# Import libraries
+import numpy as np
+import pandas as pd
+from scikit-learn.ensemble import RandomForestClassifier
+
+# Read data
+os.chdir("
